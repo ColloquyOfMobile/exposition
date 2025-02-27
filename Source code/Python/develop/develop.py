cc1d5ac301158ca80bfffda4e5e1e96a7c9739dc
monthiller <monthiller.victor@hotmail.fr>
2025-02-27 13:14:30 +0100

    Blank changes.

--- Source code/Python/develop/develop.py
+++ Source code/Python/develop/develop.py
@@ -7,12 +7,11 @@ from tests import Tests
 PARAMETERS = Parameters().as_dict()
 
 class Develop(Tests):
-    
-    classes = Tests.classes.copy()  
+
+    classes = Tests.classes.copy()
     classes["colloquy_driver"] = VirtualColloquyDriver
-    
+
     def __init__(self, wsgi):
         Tests.__init__(self, wsgi, name="develop")
         # self._doc = None
 
-    
