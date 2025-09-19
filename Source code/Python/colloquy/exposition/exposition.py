b12a0c89dffc1cd195cb06bd2e56a2fa80fe86d5
ColloquyOfMobile <colloquyofmobiles@gmail.com>
2025-09-19 14:21:24 +0200

    Update exposition.py

--- Source code/Python/colloquy/exposition/exposition.py
+++ Source code/Python/colloquy/exposition/exposition.py
@@ -51,6 +51,9 @@ class Exposition(HTMLElement):
                 doc.stag("input", type="number", name="value", value=self.near_origin_threashold, min=min_value, max=max_value, increment=1)
                 with tag("button", name="action", value=path):
                     text("set")
+                with tag("ul"):
+                    with tag("li"):
+                        text("400 => around 100 interaction per hour.")
             self.actions[path] = self.set_near_origin_threashold
             
         with tag("div"):
